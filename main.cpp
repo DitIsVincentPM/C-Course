f37aea99437146a02378fe44f1161b7394f6cfde
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-29 10:23:57 +0100

    C++ Course P. 4

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@ namespace first {
     int x = 1;
 }
 
+namespace second {
+    int x = 2;
+}
+
 int main() {
-    int x = 0;
-    std::cout << first::x << std::endl;
+    // this removes need to declare namespace
+    using std::string;
+    using std::cout;
+
+    cout << first::x + second::x << std::endl;
 
     return 0;
 }
