b05cd96d29aa0c35b926e0cc37f09aa5564ca9a1
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-29 11:02:05 +0100

    C++ Course P. 17 String

--- main.cpp
+++ main.cpp
@@ -2,25 +2,16 @@
 #include <cmath>
 
 int main() {
-    double temp;
-    char unit;
+    std::string name;
 
-    std::cout << "What is your starting unit: F = Farenhite, C = Celcius: \n";
-    std::cin >> unit;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin >> std::ws, name);
 
-    if (unit == 'C') {
-        std::cout << "Enter temperature in Celsius: \n";
-        std::cin >> temp;
-
-        temp = (1.8*temp) + 32;
-        std::cout << "It's " << temp << " farenhite.\n";
+    /*if(name.length() > 12) {
+        std::cout << "Your name is too long!\n";
     } else {
-        std::cout << "Enter temperature in Fahrenheit: \n";
-        std::cin >> temp;
-
-        temp = (temp - 32) / 1.8;
-        std::cout << "It's " << temp << " celcius.\n";
-    }
+        std::cout << "Your name is too short!\n";
+    }*/
 
     return 0;
 }
