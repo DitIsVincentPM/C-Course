2a079d464c64ba58944e4d2b6a72a25b824365f8
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-29 10:41:01 +0100

    C++ Course P. 8 User input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
-using text_t = std::string;
-
 int main() {
+    std::string name;
+    int age;
+
+    std::cout << "Enter your fullVi name: \n";
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Enter your age: \n";
+    std::cin >> age;
+
+    std::cout << "Welcome, " << name << std::endl;
+    std::cout << "You are, " << age << " years old" <<std::endl;
 
     return 0;
 }
