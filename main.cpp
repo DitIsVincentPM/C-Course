1bfec909ec74d18ad40c0f18d4e9b01e7c311967
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-29 10:28:26 +0100

    C++ Course P. 5
    using definition

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <vector>
 
-typedef std::vector<std::pair<std::string, int>> Vector2;
+using text_t = std::string;
 
 int main() {
-    Vector2 v;
 
-    v.push_back(std::make_pair("John", 20));
-    std::cout << v[0].first << " " << v[0].second << std::endl;
     return 0;
 }
