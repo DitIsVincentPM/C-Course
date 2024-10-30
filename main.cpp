0fffc2ed5e8221d9e6aa42e0c8b51b5f4a30c06d
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-30 12:15:35 +0100

    C++ Course P. 56 Objects

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 
-template <typename T, typename U>
-auto max(T x, U y) {
-    return x > y ? x : y;
-}
+class Human {
+    public:
+        std::string name;
+        std::string occupation;
+        int age;
+
+        void eat() {
+            std::cout << "This person is eating" << '\n';
+        }
+    void drink() {
+            std::cout << "This person is drink" << '\n';
+        }
+};
 
 int main() {
 
+    Human human;
+
+    human.name = "Dirk";
+    human.occupation = "Occupation";
+    human.age = 18;
+
+    human.eat();
+    human.drink();
 
     return 0;
 }
