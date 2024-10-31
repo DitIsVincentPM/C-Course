c3b927ad933c2c9b94909dde3ec83a6d807c4148
DitIsVincentPM <vanhoofv4@gmail.com>
2024-10-31 09:42:24 +0100

    C++ Rock Paper Scissors

--- main.cpp
+++ main.cpp
@@ -1,29 +1,111 @@
 #include <iostream>
+#include <stdlib.h>
+#include <thread>
 
-class Human {
-    public:
-        std::string name;
-        std::string occupation;
-        int age;
+// Rock Paper Siccors
+// Rock -> Scissors
+// Paper -> Rock
+// Scissors -> Paper
 
-        void eat() {
-            std::cout << "This person is eating" << '\n';
-        }
-    void drink() {
-            std::cout << "This person is drink" << '\n';
-        }
-};
 
 int main() {
+    int wins;
+    int loses;
+    char again;
+    int userMove;
 
-    Human human;
+    int randomNum;
 
-    human.name = "Dirk";
-    human.occupation = "Occupation";
-    human.age = 18;
+    // Set Randomize on Start program
+    srand(time(NULL));
 
-    human.eat();
-    human.drink();
+    while(true) {
+        again = ' ';
+        userMove = 0;
+        randomNum = rand() % 3 + 1;
 
+        std::cout << "Computer is thinking...\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Computer chose a move\n";
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "******* RPS *******\n";
+        std::cout << "Choose your move:\n";
+        std::cout << "1. Rock\n";
+        std::cout << "2. Paper\n";
+        std::cout << "3. Scissors\n";
+        std::cout << "******* RPS *******\n";
+        std::cin >> userMove;
+
+            switch (userMove) {
+                case 1:
+                    std::cout << "You chose Rock\n";
+                    if(randomNum == 1) {
+                        std::cout << "Computer chose: Rock\n";
+                        std::cout << "It's a tie.\n";
+                    }
+                    else if(randomNum == 2) {
+                        std::cout << "Computer chose: Paper\n";
+                        std::cout << "You lost.\n";
+                        loses++;
+                    } else if(randomNum == 3) {
+                        std::cout << "Computer chose: Scissors\n";
+                        std::cout << "You win.\n";
+                        wins++;
+                    }
+                    break;
+                case 2:
+                    std::cout << "You chose Paper\n";
+                    if(randomNum == 1) {
+                        std::cout << "Computer chose: Rock\n";
+                        std::cout << "You win.\n";
+                        wins++;
+                    }
+                    else if(randomNum == 2) {
+                        std::cout << "Computer chose: Paper\n";
+                        std::cout << "It's a tie.\n";
+                    } else if(randomNum == 3) {
+                        std::cout << "Computer chose: Scissors\n";
+                        std::cout << "You lost.\n";
+                        loses++;
+                    }
+                    break;
+                case 3:
+                    std::cout << "You chose Scissors\n";
+                    if(randomNum == 1) {
+                        std::cout << "Computer chose: Rock\n";
+                        std::cout << "You lost.\n";
+                        loses++;
+                    }
+                    else if(randomNum == 2) {
+                        std::cout << "Computer chose: Paper\n";
+                        std::cout << "You win.\n";
+                        wins++;
+                    } else if(randomNum == 3) {
+                        std::cout << "Computer chose: Scissors\n";
+                        std::cout << "It's a tie.\n";
+                    }
+                break;
+            }
+
+            std::cout << wins << " wins\n";
+            std::cout << loses << " loses\n";
+
+            while(true) {
+                std::cout << "Wanne play again? (A = Again, Q = Quit)\n";
+                std::cin >> again;
+                if(again == 'A') {
+                    break;
+                }
+
+                if(again == 'Q') {
+                    break;
+                }
+            }
+
+        if(again == 'Q') {
+            break;
+        }
+    }
     return 0;
 }
